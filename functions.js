78ba959fe9415afbf9443e87eab6573b8ebae710
ZeBin Xiao <juniorxiao01@gmail.com>
2018-05-29 11:47:18 +1200

    completed first 4 functions

--- functions.js
+++ functions.js
@@ -5,5 +5,14 @@ module.exports = {
 
   callsProperty: function (obj) {
     obj.increment()
+  },
+
+  filter: function (arr, fn) {
+   return arr.filter(fn) 
+  },
+
+  find: function (arr, fn) {
+    return arr.find(fn)
   }
 }
+
